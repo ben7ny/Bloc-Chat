bb47fd505e6a565251d4bbe8558ec321173a3420
ben7ny <benjason777@gmail.com>
2018-03-21 12:27:38 -0400

    rooms are rendered

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <RoomList firebase="{firebase}"/>
+        <RoomList firebase={firebase} />
       </div>
     );
   }
