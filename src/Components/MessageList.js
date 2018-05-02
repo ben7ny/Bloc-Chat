92f24a1ccc2fae364ff72b510f8a96e9039f89d3
ben7ny <benjason777@gmail.com>
2018-05-02 13:11:53 -0400

    done with BlocChat

--- src/Components/MessageList.js
+++ src/Components/MessageList.js
@@ -1,123 +1,5 @@
-// import React, { Component } from 'react';
-//
-//
-// class MessageList extends Component{
-//   constructor(props){
-//     super(props);
-//     this.state = {
-//       messages: [],
-//       activeMessages: [],
-//       newMessageText: ''
-//
-//     };
-//
-//     this.messageRef = this.props.firebase.database().ref('messages');
-//   }
-//
-//
-//   // createNewMessage(e){
-//   //   e.preventDefault();
-//   //   const content = this.state.content;
-//   //   this.messageRef.push({
-//   //     content: content,
-//   //     roomId: this.props.activeRoom.key,
-//   //     username: this.props.currentUser,
-//   //     sentAt: this.props.firebase.database.ServerValue.TIMESTAMP
-//   //   });
-//   //   this.setState({ content: ''});
-//   // }
-//
-//   createNewMessage(message){
-//   console.log(this.props.activeRoom);
-//   this.messageRef.push({
-//   content: message,
-//   roomId: this.props.activeRoom.key,
-//   username: this.props.currentUser,
-//   sentAt: this.props.firebase.database.ServerValue.TIMESTAMP
-//   });
-//   this.setState({ message: "" });
-//   }
-//
-//
-//
-//
-//
-//   getMessageChange(e){
-//
-//     this.setState({ content:  e.target.value });
-//
-//   }
-//
-//   updateActiveMessageList(activeRoom, message) {
-//     if(!activeRoom) {return};
-//     this.setState({activeMessages: this.state.messages.filter(message => message.roomId === activeRoom.key)})
-//   }
-//
-//
-//   componentWillReceiveProps(nextProps) {
-//     this.updateActiveMessageList(nextProps.activeRoom);
-//   }
-//
-//   componentDidMount(activeRoom) {
-//     this.messageRef.on('child_added', snapshot => {
-//       const message = snapshot.val();
-//       message.key = snapshot.key;
-//       this.setState({ messages: this.state.messages.concat( message ) })
-//     });
-//      // // let activeRoom = {this.props.activeRoom.name};
-//      //
-//      //  this.updateActiveMessageList(this.props.activeRoom)
-//
-//   }
-//
-//
-//
-//
-//   render(){
-//     return(
-//       <div className="messageParts">
-//         <h2>Active Room:{this.props.activeRoom.name}</h2>
-//         <div className="messageList">{this.state.activeMessages.map((message, index)=>
-//           <ul  key={index}>
-//             <li><h3>User:{message.username}</h3></li>
-//             <li>time:{message.sentAt}</li>
-//             <li>{message.content}</li>
-//           </ul>
-//
-//         )}
-//
-//         </div>
-//         <section className="messageForm">
-//           <form className="newMessage" onSubmit={this.createNewMessage(this.state.newMessageText)}>
-//             <label>
-//               <input type="text" placeholder="Write Your Message" value={this.state.content} onChange={(e)=>this.getMessageChange(e)}/>
-//             </label>
-//             <input type="submit" value="Send Message"/>
-//           </form>
-//         </section>
-//       </div>
-//
-//     );
-//   }
-// }
-//
-//
-//
-//
-// export default MessageList;
-
-
-
-
-
-
-
-
-
-
-
 import React, { Component } from 'react';
-import moment from 'moment';
+import Moment from 'react-moment';
 
 
 class MessageList extends Component{
@@ -189,7 +71,7 @@ class MessageList extends Component{
         <div className="messageList">{this.state.activeMessages.map((message, index)=>
           <ul  key={index}>
             <li><h3>User:{message.username}</h3></li>
-            <li>time:{message.sentAt}</li>
+            <li><Moment format='lll'>{message.sentAt}</Moment></li>
             <li>{message.content}</li>
           </ul>
 
